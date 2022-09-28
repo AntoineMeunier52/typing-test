e9b57c2d05a89ccf0137fa2324be59d76342bfb7
AntoineMeunier52 <meunier.antoine52@gmail.com>
2022-09-28 03:08:52 +0200

    handle backspace

--- front/src/typing/typing.js
+++ front/src/typing/typing.js
@@ -1,4 +1,5 @@
 const textBox = document.querySelector(".text-content");
+const inputField = document.querySelector(".text-input");
 const quote = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 
 // function getWords() {
@@ -12,7 +13,37 @@ async function getNextText() {
 		const charSpan = document.createElement('span');
 		charSpan.innerText = span;
 		textBox.appendChild(charSpan);
-	})
-}
+	});
+	document.addEventListener("keydown", () => inputField.focus());
+	textBox.addEventListener("click", () => inputField.focus());
+};
+
+let indexChar = 0;
+
+function handleInput(e) {
+	const charText = textBox.querySelectorAll("span");
+	let typedChar = e.target.value.split("")[indexChar];
+	
+	if (typedChar == null) {
+		if (indexChar != 0){
+			indexChar--
+			charText[indexChar].classList.add("active");
+			charText[indexChar + 1].classList.remove("active");
+		};
+	} else {
+		if (charText[indexChar].innerText === typedChar) {
+			charText[indexChar].classList.add("correct");
+		} else {
+			charText[indexChar].classList.add("incorrect");
+		}
+		indexChar++;
+		if (indexChar != 0) charText[indexChar - 1].classList.remove("active");
+		charText[indexChar].classList.add("active");
+	}
+	
+	console.log(indexChar);
+	console.log(e.target.value.split(""));
+};
 
 getNextText();
+inputField.addEventListener("input", handleInput)
